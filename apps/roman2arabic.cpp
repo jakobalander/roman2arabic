88dc1005983318faf36eb3ddfd6830b0940bf59e
Jakob Alander <jakob.alander@gmail.com>
2024-02-04 20:01:25 +0100

    Print error msg to stderr.

--- apps/roman2arabic.cpp
+++ apps/roman2arabic.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char** argv) {
   if (argc != 2) {
-    std::cout << "please provide a roman number to convert\n";
+    std::cerr << "please provide a roman number to convert\n";
     return EXIT_FAILURE;
   }
   try {
